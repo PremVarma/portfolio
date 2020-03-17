b2daf863d05e40407c1f665411707920f38a833d
Prem Varma <prem@eniak.xyz>
2020-03-17 16:48:23 +0530

    Updates

---
+++ precache-manifest.a5ad8b6095088101ab70bdf3b0143875.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e462d06a46a415bd25f2641aa96989",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8756686de56437be2682",
+    "url": "/portfolio/static/css/2.3d60f3ae.chunk.css"
+  },
+  {
+    "revision": "0d088add83a286d5e1ea",
+    "url": "/portfolio/static/css/main.3a293764.chunk.css"
+  },
+  {
+    "revision": "8756686de56437be2682",
+    "url": "/portfolio/static/js/2.458d928e.chunk.js"
+  },
+  {
+    "revision": "e9a45d9472c063c8a1c023664dfc3c9f",
+    "url": "/portfolio/static/js/2.458d928e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d088add83a286d5e1ea",
+    "url": "/portfolio/static/js/main.2da2198d.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "74c17afed799feb48b12bc74cc6227a6",
+    "url": "/portfolio/static/media/portfolio.74c17afe.png"
+  }
+]);
